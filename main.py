2f9c679769fab0756939e1d374316ad985b1417e
sonOfArent <humbleogier@gmail.com>
2023-03-24 20:27:40 -0500

    i don't understand

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import json
 
 namesDict = {}
+namesLst = []
 
 folder_dir = './images'
 for image in os.listdir(folder_dir):
     namesDict[image[:-4]] = image
+    namesLst.append(image[:-4])
 
 namesDictJSON = json.dumps(namesDict)
 
@@ -13,6 +15,17 @@ f = open("namesdata.json", "x")
 f.write(namesDictJSON)
 f.close()
 
-f = open("namesdata.json", "r")
-print(f.read())
+f = open("nameslst.txt", "x")
+f.close()
+
+f = open("nameslst.txt", "a")
+
+namesLst.clear()
+
+for image in os.listdir(folder_dir):
+    f.write(f"import {image[:-4]} from './images/{image}'\n")
+    namesLst.append(image)
+
+f.write(f"\n\nconst images = {namesLst}")
+
 f.close()
